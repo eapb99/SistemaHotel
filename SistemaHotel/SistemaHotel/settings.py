0110d0c59e02bacd6c3fd5bf6da2684e40d4413e
eapb99 <emaalpar@espol.edu.ec>
2025-07-16 21:49:26 -0500

    More Changes

--- SistemaHotel/SistemaHotel/settings.py
+++ SistemaHotel/SistemaHotel/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False#config('DEBUG', default=True, cast=bool)   
+DEBUG = False #config('DEBUG', default=True, cast=bool)   
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
